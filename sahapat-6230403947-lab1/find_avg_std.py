6a3118c21eb3a844c1105782099e43a05617153a
njfgyuq <69181412+njfgyuq@users.noreply.github.com>
2020-08-04 11:18:35 +0700

    Update find_avg_std.py

--- sahapat-6230403947-lab1/find_avg_std.py
+++ sahapat-6230403947-lab1/find_avg_std.py
@@ -4,5 +4,6 @@ number_one = random.randint(1, 11)
 number_two = random.randint(1, 11)
 average = number_one + number_two / 2
 standard = (statistics.pstdev([number_one, number_two]))
+
 print("The average of", number_one, "and", number_two, "is", average)
 print("The standard deviation of ", number_one, "and", number_two, "is", standard)
