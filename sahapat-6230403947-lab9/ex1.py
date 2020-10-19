b73542de322b7488caef5d73263f16583bb3cac5
Sahapat Poomunna <69181412+njfgyuq@users.noreply.github.com>
2020-10-19 13:39:43 +0700

    Add files via upload

--- sahapat-6230403947-lab9/ex1.py
+++ sahapat-6230403947-lab9/ex1.py
@@ -1,3 +1,34 @@
-from fib import fib
+from abc import ABC, abstractmethod
 
-print(fib(3))
+
+class Animal(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class Human(Animal):
+    def move(self):
+        print("I can walk and run")
+
+
+class Snake(Animal):
+    def move(self):
+        print("I can crawl")
+
+
+class Dog(Animal):
+    def move(self):
+        print("I can bark")
+
+
+if __name__ == "__main__":
+    human = Human()
+    human.move()
+    snake = Snake()
+    snake.move()
+    dog = Dog()
+    dog.move()
--- sahapat-6230403947-lab9/ex2.py
+++ sahapat-6230403947-lab9/ex2.py
@@ -1,7 +1,36 @@
-from pkg.fact import fact
-from pkg.fib_in import fib
+from abc import ABC, abstractmethod
 
 
-n = 5
-print("fact({}) is {}".format(n, fact(n)))
-print("fib({}) is {}".format(n, fib(n)))
+class Abstract_class_image:
+    @abstractmethod
+    def load_image(self):
+        pass
+
+    @abstractmethod
+    def save_image(self):
+        pass
+
+
+class Bitmap(Abstract_class_image):
+    def load_image(self, name):
+        print(f"loading bitmap file {name}")
+
+    def save_image(self, name):
+        print(f"saving bitmap file {name}")
+
+
+class Jpeg(Abstract_class_image):
+    def load_image(self, name):
+        print(f"loading bitmap file {name}")
+
+    def save_image(self, name):
+        print(f"saving bitmap file {name}")
+
+
+if __name__ == "__main__":
+    bitmap1 = Bitmap()
+    bitmap1.save_image("kku.bmp")
+    bitmap1.load_image("kku.bmp")
+    jpeg1 = Jpeg()
+    jpeg1.save_image("en.jpg")
+    jpeg1.load_image("en.jpg")
